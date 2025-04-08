873c35b70517c6377c89db48f0fb7f96b614743c
ali reza azdast <azdast47@gmail.com>
2025-04-08 17:49:21 +0330

    finished a feature section add the card tilt effect and few more things little ajustment on main.css

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,3 +1,43 @@
+import { useState, useRef , useEffect } from "react";
+import { TiLocationArrow } from "react-icons/ti";
+
+const BentoTilt = ({ children, className = "" }) => {
+  const [transformStyle, setTransformTyle] = useState("");
+  const itemRef = useRef(null);
+
+  const handleMouseMove = (event) => {
+    if (!itemRef.current) return;
+
+    const { left, top, width, height } =
+      itemRef.current.getBoundingClientRect();
+
+    const relativeX = (event.clientX - left) / width;
+    const relativeY = (event.clientY - top) / height;
+
+    const tiltX = (relativeY - 0.5) * 8;
+    const tiltY = (relativeX - 0.5) * -8;
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(.95, .95, .95)`;
+    setTransformTyle(newTransform);
+  };
+
+  const handleMouseLeave = () => {
+    setTransformTyle("");
+  };
+
+  return (
+    <div
+      ref={itemRef}
+      className={className}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ transform: transformStyle }}
+    >
+      {children}
+    </div>
+  );
+};
+
 const BentoCard = ({ src, title, description }) => {
   return (
     <div className="relative size-full">
@@ -10,7 +50,7 @@ const BentoCard = ({ src, title, description }) => {
       />
       <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
         <div>
-          <h1 className="bento-title font-ss01 spacil-font">{title}</h1>
+          <h1 className="bento-title special-font">{title}</h1>
           {description && (
             <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
           )}
@@ -21,6 +61,8 @@ const BentoCard = ({ src, title, description }) => {
 };
 
 const Features = () => {
+
+
   return (
     <section className="bg-black pb-52">
       <div className="container mx-auto px-3 md:px-10">
@@ -34,7 +76,7 @@ const Features = () => {
             experience on your world.
           </p>
         </div>
-        <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+        <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
           <BentoCard
             src="videos/feature-1.mp4"
             title={
@@ -44,9 +86,9 @@ const Features = () => {
             }
             description="The game of games app transforming moments across Web2 & Web3 titles into rewards"
           />
-        </div>
+        </BentoTilt>
         <div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
-          <div className="bento-tilt_1 border-hsla row-span-1 md:col-span-1 md:row-span-2 ">
+          <BentoTilt className="border-hsla col-span-2 row-span-1 md:col-span-1 md:row-span-2 ">
             <BentoCard
               src="videos/feature-2.mp4"
               title={
@@ -56,8 +98,8 @@ const Features = () => {
               }
               description="The NFT collection merging Zentry’s IP, AI, and gaming—pushing the boundaries of NFT innovation."
             />
-          </div>
-          <div className="bento-tilt_1 border-hsla row-span-1 md:col-span-1 ms-32 md:ms-0">
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_1 border-hsla col-span-1 md:col-span-1">
             <BentoCard
               src="videos/feature-3.mp4"
               title={
@@ -67,18 +109,34 @@ const Features = () => {
               }
               description="A gamified social hub, adding a new dimension of play to social interaction for Web3 communities."
             />
-          </div>
-          <div className="bento-tilt_1 border-hsla row-span-1 md:col-span-1 ms-32 md:ms-0">
-          <BentoCard
-            src="videos/feature-4.mp4"
-            title={
-              <>
-                az<b>u</b>l
-              </>
-            }
-            description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
-          />
-          </div>
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_1 border-hsla row-span-1 md:col-span-1 md:ms-0">
+            <BentoCard
+              src="videos/feature-4.mp4"
+              title={
+                <>
+                  az<b>u</b>l
+                </>
+              }
+              description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
+            />
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_2 flex size-full flex-col justify-between bg-violet-300 p-5">
+            <h1 className="bento-title special-font max-w-64 text-black">
+              <b>M</b>ore C<b>o</b>mi<b>n</b>g s<b>o</b>
+              <b>o</b>n!
+            </h1>
+            <TiLocationArrow className="m-5 scale-[5] self-end" />
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_2 ">
+            <video
+              src="videos/feature-5.mp4"
+              muted
+              loop
+              autoPlay
+              className="size-full object-center object-cover"
+            />
+          </BentoTilt>
         </div>
       </div>
     </section>
