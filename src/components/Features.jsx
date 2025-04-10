a2c75fe23eb901b1cddbd2119ef10164ad8a3c73
ali reza azdast <azdast47@gmail.com>
2025-04-10 13:10:39 +0330

    finished a story section there is just little more left to be down

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef , useEffect } from "react";
+import { useState, useRef } from "react";
 import { TiLocationArrow } from "react-icons/ti";
 
 const BentoTilt = ({ children, className = "" }) => {
